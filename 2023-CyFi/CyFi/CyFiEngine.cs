d9eddac518e77a46ce80f904ff8517891a97660c
kortgat <vanniekerk.charl@gmail.com>
2023-06-23 10:55:13 +0200

    Fixed loop with multiple bots
    the engine still overlaps ticks adding a simple in loop check it processes 4 bots without issue

--- 2023-CyFi/CyFi/CyFiEngine.cs
+++ 2023-CyFi/CyFi/CyFiEngine.cs
@@ -322,10 +322,16 @@ namespace CyFi
             TickTimer.AutoReset = true;
         }
 
+ bool inLoop = false;
         private void OnTimedEvent(object? sender, ElapsedEventArgs e)
         {
-            Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
-            GameLoop();
+             if (!inLoop)
+            {
+                inLoop = true;
+                Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+                GameLoop();
+                inLoop = false;
+            }
         }
     }
 }
